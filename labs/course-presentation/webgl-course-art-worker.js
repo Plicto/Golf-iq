23699440f08668b05596f4824809f2860778fc32
Plicto <38166482+Plicto@users.noreply.github.com>
2026-08-19 17:59:40 +0200

    Preview integrated stream v17

--- labs/course-presentation/webgl-course-art-worker.js
+++ labs/course-presentation/webgl-course-art-worker.js
@@ -11,7 +11,7 @@ import {
   createVisualWaterTerrainWorld,
   createVisualWatercourseWorld,
   replaceVisualWatercourseGeometry,
-} from "./webgl-watercourse-visual-v16.js";
+} from "./webgl-watercourse-visual-v17.js";
 
 const assertRequest = (message) => {
   const identity = message?.identity;
