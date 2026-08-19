a874e9b688092bfc39cd451a01010580f554ce4f
Plicto <38166482+Plicto@users.noreply.github.com>
2026-08-19 19:57:53 +0200

    Use consolidated PR225 watercourse module

--- labs/course-presentation/webgl-course-art-worker.js
+++ labs/course-presentation/webgl-course-art-worker.js
@@ -11,7 +11,7 @@ import {
   createVisualWaterTerrainWorld,
   createVisualWatercourseWorld,
   replaceVisualWatercourseGeometry,
-} from "./webgl-watercourse-visual-v19.js";
+} from "./webgl-watercourse-visual.js";
 
 const assertRequest = (message) => {
   const identity = message?.identity;
